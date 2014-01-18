ba68b6499ad2dc5b6d540846a3d3f5cac5bab523
Kyle Cline <kyledcline@gmail.com>
2014-01-18 10:46:32 -0600

    Debug comm

--- jspgsql.js
+++ jspgsql.js
@@ -15,6 +15,7 @@ function jspgQuery(ipAddInt, wsData)
         onSuccess:  function(transport) {
             if ((jspgOption["output_type"] == "json") && (transport.responseText.length > 0)) {
                 ajax_out = transport.responseText;
+                console.log(ajax_out);
                 // ajax_out = ajax_out.evalJSON();
                 manageNewTX(ajax_out, wsData);
             }
